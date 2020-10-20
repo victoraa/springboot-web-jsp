3c4469da850a52e1534ea58a8e563d815c7ec754
Victor <victor.andres1@gmail.com>
2020-10-20 21:41:16 +0200

    Init

---
+++ src/main/java/com/viewnext/HelloController.java
@@ -0,0 +1,18 @@
+package com.viewnext;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping({ "/", "/hello" })
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("name", name);
+		model.addAttribute("mensaje", "Mucho gusto en saludarte");
+		return "welcome";
+	}
+}
